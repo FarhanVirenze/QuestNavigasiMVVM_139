dd3d9ddc39e0cab70be9cc331b48872ebf814423
farhan <farhanvirenze18@gmail.com>
2024-11-21 21:05:59 +0700

    update gradle.app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pam.questnavigasimvvm_139"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pam.questnavigasimvvm_139"
