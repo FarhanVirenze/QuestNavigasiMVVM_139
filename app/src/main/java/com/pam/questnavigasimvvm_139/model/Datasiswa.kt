0505546d014f392d7138b4a4c7ccab7a3e43af9a
farhan <farhanvirenze18@gmail.com>
2024-11-21 21:14:09 +0700

    update val nim

--- app/src/main/java/com/pam/questnavigasimvvm_139/model/Datasiswa.kt
+++ app/src/main/java/com/pam/questnavigasimvvm_139/model/Datasiswa.kt
@@ -1,6 +1,7 @@
 package com.pam.questnavigasimvvm_139.model
 
 data class Datasiswa(
+    val nim: String = "",
     val nama: String = "",
     val alamat: String = "",
     val gender: String = "",
