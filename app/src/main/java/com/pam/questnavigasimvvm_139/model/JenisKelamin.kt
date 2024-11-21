1aee2ca23c242bd6c4aa50719978baddcdd57908
farhan <farhanvirenze18@gmail.com>
2024-11-21 20:50:49 +0700

    Membuat Object JenisKelamin.kt untuk radiobutton

---
+++ app/src/main/java/com/pam/questnavigasimvvm_139/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.pam.questnavigasimvvm_139.model
+
+import com.pam.questnavigasimvvm_139.R
+
+object JenisKelamin {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
