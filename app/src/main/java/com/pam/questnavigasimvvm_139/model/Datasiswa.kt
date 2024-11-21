c1f0d936c8b156cc3a6ee85444eb12939c80bcb5
farhan <farhanvirenze18@gmail.com>
2024-11-21 20:49:01 +0700

    Membuat data class Datasiswa.kt

---
+++ app/src/main/java/com/pam/questnavigasimvvm_139/model/Datasiswa.kt
@@ -0,0 +1,9 @@
+package com.pam.questnavigasimvvm_139.model
+
+data class Datasiswa(
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val notelp: String = ""
+)
